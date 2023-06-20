6f13786f8b21b1a99e4528d98745f56e942fc768
12345sunil2003 <113965500+12345sunil2003@users.noreply.github.com>
2023-06-20 13:00:59 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,69 @@
+package factor;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Scanner;
+
+	class fact	{
+
+	public static void main(String[] args)
+
+	{
+
+	int i,j,pr=1;
+
+	try{
+
+	Scanner s=new Scanner(System.in);
+
+	System.out.println("Enter the number to find the factorial");
+
+	int n=s.nextInt();
+
+	if(n<0)
+
+	{
+
+	System.out.println("Invalid");
+
+	}
+
+	else if(n==0)
+
+	{
+
+	System.out.println("1");
+
+	}
+
+	else
+
+	{
+
+	for(i=n;i>0;i--)
+
+	{
+
+	pr=pr*i;
+
+	}
+
+	System.out.println("The answer is:"+pr);
+
+	assertTrue(120==pr);
+
+	}
+
+	}
+
+	catch(Exception e)
+
+	{
+
+	System.out.println("Invalid");
+
+	}
+
+	}
+
+	}
